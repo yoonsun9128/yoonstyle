bf93a9717acafb52b97ef196ef769d2738cb767d
yoonsun9128 <yoonsun9128@gmail.com>
2022-10-11 13:33:38 +0900

    user models

--- users/models.py
+++ users/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class UserModel(models.Model):
+    class Meta:
+        db_table = "my_user"
+    username = models.CharField(max_length=10, null=False)
+    name = models.CharField(max_length=10, null=False)
+    password = models.CharField(max_length=15, null=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
