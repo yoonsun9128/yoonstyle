f5bce3ddb68864f38eeaf42ad58014e39446327d
yoonsun9128 <yoonsun9128@gmail.com>
2022-10-11 19:12:21 +0900

    sign up system

--- users/urls.py
+++ users/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    # path('sing-up/', views.sign_up, name='sing_up')
-
+    path('sign-up/', views.sign_up, name='sing_up'), #회원가입
+    path('log-in/', views.log_in, name='log_in') #로그인화면
 ]
--- users/views.py
+++ users/views.py
@@ -1,6 +1,29 @@
 from django.shortcuts import render
+from .models import UserModel
 
 # Create your views here.
-# def sign_up (request):
-#     if request.method == 'GET':
+def log_in(request):
+    return render(request, 'login.html')
+
+def sign_up (request):
+    if request.method == 'GET':
+        return render(request, 'signup.html')
+    elif request.method == 'POST':
+        username = request.POST.get('username')
+        name = request.POST.get('name')
+        password = request.POST.get('password')
+        password2 = request.POST.get('password2')
         
+        if password != password2:
+            return render(request, 'signup.html', {'error':'패스워드를 확인해 주세요'})
+        else:
+            exist_guest = UserModel.objects.filter(username=username)
+            if exist_guest:
+                return render(request, 'signup.html', {'error':'이미 존재하는 유저입니다'})
+            else:
+                new_guest = UserModel()
+                new_guest.username = username
+                new_guest.name = name
+                new_guest.password = password
+                new_guest.save()
+                return render(request, 'login.html')
